737b40c5945e939300cdf6f8d31e7301664ceb26
Devran Perdana Malik <devranmalik82@gmail.com>
2026-01-15 22:01:47 +0700

    Configure Next.js image settings to allow remote images from Freepik

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [{ protocol: 'https', hostname: 'img.freepik.com' }],
+  },
 };
 
 export default nextConfig;
