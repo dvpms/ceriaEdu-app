cdf9aafde5910ab1d5a036ad013a50c5bf08b178
Devran Perdana Malik <devranmalik82@gmail.com>
2026-01-15 21:38:59 +0700

    Update README and configuration files; add Tailwind CSS setup and dependencies

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: [
+    "./src/pages/**/*.{js,jsx,mdx}",
+    "./src/components/**/*.{js,jsx,mdx}",
+    "./src/app/**/*.{js,jsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
